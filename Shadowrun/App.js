11be696199f0dd8623b04774aedcd195a939188d
carlosccj <carlosccj@uma.es>
2023-02-12 19:20:51 +0100

    Refactored the testMetatype method (now called adjustMetatype) to fit the Single Responsability OOP Principle

--- Shadowrun/App.js
+++ Shadowrun/App.js
@@ -9,7 +9,6 @@ const rows = selectorGridDimensions
 const cols = rows
 
 let selectedCellCoordinates = []
-let displacedCellCoordinates = []
 let metaPoints = 0
 
 createGrid()
@@ -35,17 +34,14 @@ function changeCell(cellSelected) {
     const selectedCellColNumber = cellSelected % selectorGridDimensions
     selectedCellCoordinates.push(selectedCellRowNumber, selectedCellColNumber)
     changeColor(cellSelected, selectedCellRowNumber, selectedCellColNumber)
-    testMetatype(cellSelected)
-    testNature(cellSelected)
-    testNuyen(cellSelected)
+    adjustMetatype()
+    adjustNature()
+    adjustNuyen()
     selectedCellCoordinates.length = 0
-    displacedCellCoordinates.length = 0
 }
 
 function changeColor(cellSelected, selectedCellRowNumber, selectedCellColNumber) {
-    const displacedCellRowNumber = adjustColumn(selectedCellRowNumber, selectedCellColNumber, cellSelected)
-    const displacedCellColNumber = adjustRow(selectedCellRowNumber, selectedCellColNumber, displacedCellRowNumber)
-    displacedCellCoordinates.push(displacedCellRowNumber, displacedCellColNumber)
+    adjustColumn(selectedCellRowNumber, selectedCellColNumber, cellSelected)
 }
 
 function adjustColumn(selectedCellRowNumber, selectedCellColNumber, cellSelected) {
@@ -61,12 +57,11 @@ function adjustColumn(selectedCellRowNumber, selectedCellColNumber, cellSelected
             result = i
         }
     }
-    return result
+    adjustRow(selectedCellRowNumber, selectedCellColNumber, result)
 }
 
 function adjustRow(selectedCellRowNumber, selectedCellColNumber, displacedCellRowNumber) {
     let found = false
-    let result
     for (let j = 0; j < cols && !found; j++) {
         if (cellsArray[selectedCellRowNumber][j] == true && j != selectedCellColNumber) {
             found = true
@@ -74,46 +69,59 @@ function adjustRow(selectedCellRowNumber, selectedCellColNumber, displacedCellRo
             cellsArray[selectedCellRowNumber][j] = false
             cellsDoc[selectorGridDimensions * displacedCellRowNumber + j].style.backgroundColor = selectorPurple
             cellsArray[displacedCellRowNumber][j] = true
-            result = j
-        }
-    }
-    return result
-}
-
-function testMetatype(cellSelected) {
-    if (selectedCellCoordinates[1] == 0 || displacedCellCoordinates[1] == 0) {
-        let regex = /\d+/
-        let raceCell
-        let cellStr = ""
-        let raceSelectorDoc = document.getElementById('race')
-        
-        if (selectedCellCoordinates[1] == 0) raceCell = selectedCellCoordinates[0]
-        else if (displacedCellCoordinates[1] == 0) raceCell = displacedCellCoordinates[0]
-        cellStr = cellsDoc[cellSelected].innerHTML
-        metaPoints = parseInt(cellStr.match(regex))
-
-        if ((raceCell == 0 || raceCell == 1) && raceSelectorDoc.options.length > 3) {
-            raceSelectorDoc.remove(4)
-            raceSelectorDoc.remove(3)
-        } else if ((raceCell == 2 || raceCell == 3 || raceCell == 4) && raceSelectorDoc.options.length < 5) {
-            let optionHuman = document.createElement('option')
-            optionHuman.text = "Human"
-            optionHuman.value = "H"
-            let optionElf = document.createElement('option')
-            optionElf.text = "Elf"
-            optionElf.value = "E"
-            raceSelectorDoc.add(optionHuman)
-            raceSelectorDoc.add(optionElf)
         }
     }
 }
 
-function testNature(cellSelected) {
+function adjustMetatype() {
+    const metatypeColumn = 0
+    const metatypeRow = getRow(metatypeColumn)
+    const metatypeCell = selectorGridDimensions * metatypeRow + metatypeColumn
+    setMetaPoints(metatypeCell)
+    changeMetatypeOption(metatypeRow)
+}
+
+function setMetaPoints(metatypeCell) {
+    const regex = /\d+/
+    let cellText = cellsDoc[metatypeCell].innerHTML
+    metaPoints = parseInt(cellText.match(regex))
+}
+
+function changeMetatypeOption(metatypeRow) {
+    let raceSelectorDoc = document.getElementById('race')
+    if ((metatypeRow == 0 || metatypeRow == 1) && raceSelectorDoc.options.length > 3) {
+        raceSelectorDoc.remove(4)
+        raceSelectorDoc.remove(3)
+    } else if ((metatypeRow == 2 || metatypeRow == 3 || metatypeRow == 4) && raceSelectorDoc.options.length < 5) {
+        let optionHuman = document.createElement('option')
+        optionHuman.text = "Human"
+        optionHuman.value = "H"
+        let optionElf = document.createElement('option')
+        optionElf.text = "Elf"
+        optionElf.value = "E"
+        raceSelectorDoc.add(optionHuman)
+        raceSelectorDoc.add(optionElf)
+    }
+}
+
+function adjustNature() {
 
 }
 
-function testNuyen(cellSelected) {
+function adjustNuyen() {
 
+}
+
+function getRow(metatypeColumn) {
+    let found = false
+    let row = 0 
+    for (let i = 0; row < selectorGridDimensions && !found; i++) {
+        if (cellsArray[i][metatypeColumn] == true) {
+            row = i
+            found = true
+        }
+    }
+    return row
 }
 
 function createCellsArray() {
